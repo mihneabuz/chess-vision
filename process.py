971e4785359db5e8ac0959022c284eefe82d2c2b
mihneabuz <mihneabuzatu88@gmail.com>
2022-04-25 15:08:10 +0300

    trying k-clustering

--- process.py
+++ process.py
@@ -1,15 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from cv2 import imread
-import cv2
+from cv2 import cv2 
+from sklearn.cluster import KMeans
+from random import randint
+
+def process_kmeans(im):
+    gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+
+    corners = cv2.goodFeaturesToTrack(gray, 150, 0.1, 30)
+    points = corners[:, 0, :].astype(np.uint32)
+
+    model = KMeans(n_clusters=64).fit(points)
+    colors = [(randint(0, 256), randint(0, 256), randint(0, 256)) for _ in range(64)]
+    for point, cluster in zip(points, model.predict(points)):
+        print(point, cluster, colors[cluster])
+        cv2.circle(im, point, 6, colors[cluster], -1)
+
+    plt.imshow(im[:,:,::-1])
+    plt.show()
+
+    return gray
 
 def process(im):
     gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
 
-    corners = cv2.goodFeaturesToTrack(gray, 128, 0.1, 30)
-    for corner in corners:
-        point = corner[0].astype(np.uint32)
-        print(point)
+    corners = cv2.goodFeaturesToTrack(gray, 150, 0.1, 30)
+    points = corners[:, 0, :].astype(np.uint32)
+
+    for point in points:
         cv2.circle(im, point, 6, (0, 0, 255), -1)
 
     plt.imshow(im[:,:,::-1])
@@ -17,13 +35,12 @@ def process(im):
 
     return gray
 
+
 def test():
-    print('testing crop...')
     plt.rcParams["figure.figsize"] = (10,10)
-    im = imread('boards/1003.jpeg')
-    plt.imshow(im[:,:,::-1])
-    plt.show()
-    im = process(im)
+    im = cv2.imread('boards/1009.jpg')
+    # process(im)
+    process_kmeans(im)
 
 if __name__ == '__main__':
     test()
