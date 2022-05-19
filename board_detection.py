0731cdc2b18fb2bde70f573722f5c2138f6e4c52
mihneabuz <mihneabuzatu88@gmail.com>
2022-05-19 22:26:44 +0300

    added some manual evaluation

--- board_detection.py
+++ board_detection.py
@@ -1,6 +1,8 @@
 import torch
 from torchvision import models, transforms
 from torch.utils.data import random_split, DataLoader
+from cv2 import cv2
+import matplotlib.pyplot as plt
 
 from load_data import load_data
 from utils import get_device, train_loop, validation_metrics, summary, dataset
@@ -8,7 +10,8 @@ from utils import get_device, train_loop, validation_metrics, summary, dataset
 normalize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 
 def tensor_transform(image):
-    return torch.from_numpy(image.transpose(2, 0, 1))
+    resized = cv2.resize(image, (640, 640))
+    return torch.from_numpy(resized.transpose(2, 0, 1))
 
 def jit_transform(x):
     return normalize(x / 255)
@@ -35,32 +38,45 @@ def create_model(pretrained=True):
     model.classifier.append(torch.nn.Linear(in_features=last_layer_size, out_features=8))
     return model
 
+def load_model():
+    model = create_model(pretrained=False)
+    model.load_state_dict(torch.load('./detection_weights'))
+    return model
+
 def loss_func(predicted, real):
     grouped = torch.stack((predicted[:, 0:2], predicted[:, 2:4], predicted[:, 4:6], predicted[:, 6:8]), 1)
     return (grouped - real).pow(2).sum(2).sqrt().sum()
 
-def train(limit=-1, lr=0.0001, epochs=1, batch_size=8):
+def train(epochs, lr=0.00004, batch_size=4, limit=-1, load_dict=False):
     device = get_device()
 
     train_ds, valid_ds = load_datasets(limit=limit)
     train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
     valid_dl = DataLoader(valid_ds, batch_size=batch_size)
 
-    # from cv2 import cv2
-    # import matplotlib.pyplot as plt
-    # for image, corners in train_ds:
-    #     im = image.numpy().transpose(1, 2, 0)
+    i = 0
+    for image, corners in train_ds:
+        im = image.numpy().transpose(1, 2, 0)
 
-    #     cv2.circle(im, [int(corners[0][1] * 1280), int(corners[0][0] * 1280)], 20, (0, 0, 255), 8)
-    #     cv2.circle(im, [int(corners[1][1] * 1280), int(corners[1][0] * 1280)], 20, (255, 0, 0), 8)
-    #     cv2.circle(im, [int(corners[2][1] * 1280), int(corners[2][0] * 1280)], 20, (0, 255, 255), 8)
-    #     cv2.circle(im, [int(corners[3][1] * 1280), int(corners[3][0] * 1280)], 20, (0, 255, 0), 8)
+        cv2.circle(im, [int(corners[0][1] * 640), int(corners[0][0] * 640)], 10, (0, 0, 255), 6)
+        cv2.circle(im, [int(corners[1][1] * 640), int(corners[1][0] * 640)], 10, (255, 0, 0), 6)
+        cv2.circle(im, [int(corners[2][1] * 640), int(corners[2][0] * 640)], 10, (0, 255, 255), 6)
+        cv2.circle(im, [int(corners[3][1] * 640), int(corners[3][0] * 640)], 10, (0, 255, 0), 6)
 
-    #     plt.imshow(im)
-    #     plt.show()
+        plt.imshow(im)
+        plt.show()
+
+        i += 1
+        if (i > 5):
+            break
            
-    model = create_model()
-    # summary(model, (3, 1280, 1280))
+    if load_dict:
+        model = load_model() 
+    else:
+        model = create_model()
+
+    model.to(device)
+    summary(model, (3, 640, 640))
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = loss_func
@@ -72,5 +88,31 @@ def train(limit=-1, lr=0.0001, epochs=1, batch_size=8):
         print(f'loss: {sum(curr_losses)}')
         losses += curr_losses
 
+    plt.plot(losses)
+    plt.show()
+
+    i = 0
+    model.eval()
+    for image, corners in valid_ds:
+        preds = model(jit_transform(image)[None, :, :, :])
+        im = image.numpy().transpose(1, 2, 0)
+
+        cv2.circle(im, [int(corners[0][1] * 640), int(corners[0][0] * 640)], 10, (0, 0, 255), 4)
+        cv2.circle(im, [int(corners[1][1] * 640), int(corners[1][0] * 640)], 10, (255, 0, 0), 4)
+        cv2.circle(im, [int(corners[2][1] * 640), int(corners[2][0] * 640)], 10, (0, 255, 255), 4)
+        cv2.circle(im, [int(corners[3][1] * 640), int(corners[3][0] * 640)], 10, (0, 255, 0), 4)
+
+        cv2.circle(im, [int(preds[0][0] * 640), int(preds[0][1] * 640)], 15, (0, 0, 155), 4)
+        cv2.circle(im, [int(preds[0][2] * 640), int(preds[0][3] * 640)], 15, (155, 0, 0), 4)
+        cv2.circle(im, [int(preds[0][4] * 640), int(preds[0][5] * 640)], 15, (0, 155, 155), 4)
+        cv2.circle(im, [int(preds[0][6] * 640), int(preds[0][7] * 640)], 15, (0, 155, 0), 4)
+
+        plt.imshow(im)
+        plt.show()
+
+        i += 1
+        if (i > 10):
+            break
+
 if __name__ == '__main__':
-    train(limit=10, epochs=4)
+    train(limit=10, epochs=4, load_dict=False)
