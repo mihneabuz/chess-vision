d3f7bb4eb5d0ce35bc6e5d9114147e2a4162191c
mihneabuz <mihneabuzatu88@gmail.com>
2022-09-08 01:33:34 +0300

    added validation and trying different models, smaller images

--- board_detection.py
+++ board_detection.py
@@ -3,14 +3,17 @@ from torchvision import models, transforms
 from torch.utils.data import random_split, DataLoader
 import cv2
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from load_data import load_data
 from utils import get_device, train_loop, dataset
 
 normalize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 
+size = 200
+
 def tensor_transform(image):
-    resized = cv2.resize(image, (640, 640))
+    resized = cv2.resize(image, (size, size))
     return torch.from_numpy(resized.transpose(2, 0, 1))
 
 def jit_transform(x):
@@ -32,16 +35,16 @@ def load_datasets(limit=-1):
     print(f'image size: {images[0].shape}')
 
     size = len(images)
-    train_size = int(size * 0.7)
+    train_size = int(size * 0.8)
     valid_size = size - train_size
 
     return random_split(dataset(images, corners), [train_size, valid_size])
 
 def create_model(pretrained=True):
     if pretrained:
-        model = models.efficientnet_b3(weights=models.EfficientNet_B3_Weights.DEFAULT)
+        model = models.efficientnet_v2_m(weights=models.EfficientNet_V2_M_Weights.IMAGENET1K_V1)
     else:
-        model = models.efficientnet_b3()
+        model = models.efficientnet_v2_m()
     last_layer_size = model.classifier[-1].__getattribute__('out_features')
     model.classifier.append(torch.nn.Linear(in_features=last_layer_size, out_features=8))
     return model.half()
@@ -71,16 +74,16 @@ def train(epochs, lr=0.001, batch_size=4, limit=-1, load_dict=False):
     for image, corners in train_ds:
         im = image.numpy().transpose(1, 2, 0)
 
-        cv2.circle(im, (int(corners[0][1] * 640), int(corners[0][0] * 640)), 2, (255, 0, 0), 6)
-        cv2.circle(im, (int(corners[1][1] * 640), int(corners[1][0] * 640)), 2, (0, 255, 0), 6)
-        cv2.circle(im, (int(corners[2][1] * 640), int(corners[2][0] * 640)), 2, (0, 0, 255), 6)
-        cv2.circle(im, (int(corners[3][1] * 640), int(corners[3][0] * 640)), 2, (0, 255, 255), 6)
+        cv2.circle(im, (int(corners[0][1] * size), int(corners[0][0] * size)), 2, (255, 0, 0), 2)
+        cv2.circle(im, (int(corners[1][1] * size), int(corners[1][0] * size)), 2, (0, 255, 0), 2)
+        cv2.circle(im, (int(corners[2][1] * size), int(corners[2][0] * size)), 2, (0, 0, 255), 2)
+        cv2.circle(im, (int(corners[3][1] * size), int(corners[3][0] * size)), 2, (0, 255, 255), 2)
 
         plt.imshow(im)
         plt.show()
 
         i += 1
-        if (i > 5):
+        if (i > 2):
             break
 
     if load_dict:
@@ -90,18 +93,15 @@ def train(epochs, lr=0.001, batch_size=4, limit=-1, load_dict=False):
 
     model.to(device)
 
+    optimizer = torch.optim.Adam(model.parameters(), eps=1e-05, lr=lr)
     criterion = loss_func
 
     if epochs > 5:
-        optimizer = torch.optim.Adam(model.parameters(), eps=1e-05, lr=lr * 3)
-
         set_grad(model.features, False)
         for i in range(4):
             curr_losses = train_loop(model, train_dl, optimizer, criterion, transform=jit_transform)
             print(f'loss: {sum(curr_losses)}')
         set_grad(model.features, True)
-
-    optimizer = torch.optim.Adam(model.parameters(), eps=1e-05, lr=lr)
 
     losses = []
     for i in range(epochs):
@@ -113,27 +113,33 @@ def train(epochs, lr=0.001, batch_size=4, limit=-1, load_dict=False):
     plt.plot(losses)
     plt.show()
 
-    i = 0
     model.eval()
+    valid_losses = []
+    for images, labels in tqdm(valid_dl, desc='validation'):
+        preds = model(jit_transform(images.to(device)))
+        valid_losses.append(criterion(preds, labels.to(device)).item())
+    print(f'validation loss: {sum(valid_losses)}')
+
+    i = 0
     for image, corners in valid_ds:
         preds = model(jit_transform(image)[None, :, :, :].to(device))
         im = image.numpy().transpose(1, 2, 0)
 
-        cv2.circle(im, [int(corners[0][1] * 640), int(corners[0][0] * 640)], 2, (255, 0, 0), 6)
-        cv2.circle(im, [int(corners[1][1] * 640), int(corners[1][0] * 640)], 2, (0, 255, 0), 6)
-        cv2.circle(im, [int(corners[2][1] * 640), int(corners[2][0] * 640)], 2, (0, 0, 255), 6)
-        cv2.circle(im, [int(corners[3][1] * 640), int(corners[3][0] * 640)], 2, (0, 255, 255), 6)
+        cv2.circle(im, [int(corners[0][1] * size), int(corners[0][0] * size)], 2, (255, 0, 0), 2)
+        cv2.circle(im, [int(corners[1][1] * size), int(corners[1][0] * size)], 2, (0, 255, 0), 2)
+        cv2.circle(im, [int(corners[2][1] * size), int(corners[2][0] * size)], 2, (0, 0, 255), 2)
+        cv2.circle(im, [int(corners[3][1] * size), int(corners[3][0] * size)], 2, (0, 255, 255), 2)
 
-        cv2.circle(im, [int(preds[0][1] * 640), int(preds[0][0] * 640)], 10, (255, 0, 0), 4)
-        cv2.circle(im, [int(preds[0][3] * 640), int(preds[0][2] * 640)], 10, (0, 255, 0), 4)
-        cv2.circle(im, [int(preds[0][5] * 640), int(preds[0][4] * 640)], 10, (0, 0, 255), 4)
-        cv2.circle(im, [int(preds[0][7] * 640), int(preds[0][6] * 640)], 10, (0, 255, 255), 4)
+        cv2.circle(im, [int(preds[0][1] * size), int(preds[0][0] * size)], 6, (255, 0, 0), 2)
+        cv2.circle(im, [int(preds[0][3] * size), int(preds[0][2] * size)], 6, (0, 255, 0), 2)
+        cv2.circle(im, [int(preds[0][5] * size), int(preds[0][4] * size)], 6, (0, 0, 255), 2)
+        cv2.circle(im, [int(preds[0][7] * size), int(preds[0][6] * size)], 6, (0, 255, 255), 2)
 
         plt.imshow(im)
         plt.show()
 
         i += 1
-        if (i > 10):
+        if (i > 15):
             break
 
     torch.save(model.state_dict(), './detection_weights');
