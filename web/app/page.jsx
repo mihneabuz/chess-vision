9834046911c58014e88f51994b14e414f020ea7c
mihneabuz <mihneabuzatu88@gmail.com>
2023-02-19 01:13:00 +0200

    Using new app directory

---
+++ web/app/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div className='flex flex-col justify-center'>
+      <div className='flex items-center justify-center'>
+        <button type="" onClick={() => console.log('upload!')}>
+          Upload image
+        </button>
+      </div>
+    </div>
+  )
+}
