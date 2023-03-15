0f883a55686c09f642e6c825a33feb3ecd29feac
mihneabuz <mihneabuzatu88@gmail.com>
2023-03-15 17:14:25 +0200

    working on displaying result

---
+++ web/app/api/check/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  return NextResponse.json({
+    success: false,
+  });
+}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,55 @@
 'use client';
 
-import { MouseEventHandler, ChangeEventHandler } from 'react';
+import { MouseEventHandler, ChangeEventHandler, useEffect } from 'react';
 import { useRef, useState } from 'react';
 
-export default function Home() {
-  const [imageFile, setImageFile] = useState<File | null>(null);
-  const imagePreview = useRef<HTMLImageElement>(null);
+interface Upload {
+  type: 'upload'
+};
+interface Waiting {
+  type: 'waiting',
+  id: string
+};
+interface Result {
+  type: 'result',
+  pieces: number[]
+};
 
-  const canUpload = imageFile !== null;
+type State = Upload | Waiting | Result;
+
+export default function Home() {
+  const [state, setState] = useState<State>({ type: 'upload' });
+
+  useEffect(() => {
+    if (state.type === 'waiting') {
+      const interval = setInterval(async () => {
+        const req = await fetch('/api/check');
+        const result = await req.json();
+
+        if (result.success) {
+          setState({ type: 'result', pieces: result.pieces });
+        }
+      }, 500);
+
+      return () => clearInterval(interval);
+    }
+  }, [state]);
+
+  const handleUpload = (id: string) => {
+    setState({ type: 'waiting', id });
+  };
+
+  return (
+    <div className="grid h-full place-items-center">
+      <Upload onUpload={handleUpload} canUpload={state.type === 'upload'}/>
+      <Progress message={'Processing'} hidden={state.type !== 'waiting'}/>
+      <Result pieces={state.type === 'result' ? state.pieces : []} hidden={state.type !== 'result'}/>
+    </div>
+  );
+}
+
+function Upload({ onUpload, canUpload }) {
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
     e.preventDefault();
@@ -23,7 +65,9 @@ export default function Home() {
 
       const result = await req.json();
 
-      console.log(result);
+      if (result.success) {
+        onUpload(result.id);
+      }
     }
   };
 
@@ -38,15 +82,31 @@ export default function Home() {
     }
   };
 
+  const imagePreview = useRef<HTMLImageElement>(null);
+
   return (
-    <div className="grid h-full place-items-center">
-      <form className="flex flex-col items-center">
-        <input type="file" accept="image/*" onChange={handleChange} />
-        <img className="aspect-auto w-3/4" ref={imagePreview} />
-        <button onClick={handleClick} disabled={!canUpload}>
-          Upload image
-        </button>
-      </form>
+    <form className="flex flex-col items-center">
+      <input type="file" accept="image/*" onChange={handleChange} disabled={!canUpload}/>
+      <img className="aspect-auto w-3/4" ref={imagePreview} />
+      <button onClick={handleClick} disabled={!canUpload || imageFile === null}>
+        Upload image
+      </button>
+    </form>
+  )
+}
+
+function Progress({ message, hidden }) {
+  return (
+    <div hidden={hidden}>
+      <span>{message}</span>
+    </div>
+  );
+}
+
+function Result({ pieces, hidden }) {
+  return (
+    <div hidden={hidden}>
+      <span>{pieces}</span>
     </div>
   );
 }
