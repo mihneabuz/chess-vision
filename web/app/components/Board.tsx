74008fc8d7817157533895ef8ba34d1c310c7f88
mihneabuz <mihneabuzatu88@gmail.com>
2023-03-30 19:52:37 +0300

    added picker to change board after inference

--- web/app/components/Board.tsx
+++ web/app/components/Board.tsx
@@ -6,17 +6,29 @@ import { pieces, startPosition } from 'app/pieces/util';
 
 export default function Board({ initial }) {
   const [pieces, setPieces] = useState<number[]>(initial || startPosition);
+  const [picker, setPicker] = useState<[number, number, number] | null>(null);
   const sounds = useSounds();
 
-  const handleClick = (index: number) => {
-    console.log(index);
+  const handleClick = (e: MouseEvent, index: number) => {
+    setPicker((old) => (old ? null : [e.clientX, e.clientY, index]));
+  };
+
+  const handlePick = (type: number) => {
+    if (!picker) return;
+    const square = picker[2];
+    sounds.thock1.play();
+    setPieces((pieces) => pieces.map((val, idx) => (idx === square ? type : val)));
+    setPicker(null);
   };
 
   return (
-    <div className="grid grid-cols-8 rounded border-4 border-kashmir-800">
-      {pieces.map((type, index) => (
-        <Piece key={`p${index}`} index={index} type={type} onClick={handleClick} />
-      ))}
+    <div>
+      <div className="grid grid-cols-8 rounded border-4 border-kashmir-800">
+        {pieces.map((type, index) => (
+          <Piece key={`p${index}`} index={index} type={type} onClick={handleClick} />
+        ))}
+      </div>
+      <Picker onClick={handlePick} x={picker && picker[0]} y={picker && picker[1]} />
     </div>
   );
 }
@@ -28,12 +40,51 @@ function Piece({ index, type, onClick }) {
   return (
     <div
       className={`flex aspect-square min-w-[4rem] items-center justify-center ${color}`}
-      onClick={() => onClick(index)}
+      onClick={(e) => onClick(e, index)}
     >
-      {piece
-        ? <img className="w-full opacity-80" src={piece.src} />
-        : <div className="border-none" />
-      }
+      {piece ? (
+        <img className="w-full opacity-80" src={piece.src} />
+      ) : (
+        <div className="border-none" />
+      )}
+    </div>
+  );
+}
+
+function Picker({ x, y, onClick }) {
+  const hidden = !x || !y;
+
+  const style = {
+    position: 'fixed',
+    left: `${x || 0}px`,
+    top: `${y || 0}px`,
+    opacity: hidden ? '0' : '95',
+    display: hidden ? 'none' : undefined,
+  };
+
+  return (
+    <div
+      className="grid w-32 grid-cols-2 place-items-center
+          rounded border-2 border-kashmir-800 bg-kashmir-300
+          transition-opacity"
+      style={style as any}
+    >
+      {Array.from(pieces.entries()).map(([key, piece]) => (
+        <button
+          key={key}
+          className="flex w-full justify-center rounded transition-colors hover:bg-kashmir-200"
+          onClick={() => onClick(key)}
+        >
+          <img className="aspect-square w-full" src={piece.src} />
+        </button>
+      ))}
+      <button
+        key="clear"
+        className="col-span-2 w-full rounded py-2 font-bold text-kashmir-900 transition-colors hover:bg-kashmir-200"
+        onClick={() => onClick(0)}
+      >
+        Clear
+      </button>
     </div>
   );
 }
